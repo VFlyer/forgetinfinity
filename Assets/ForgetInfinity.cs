5560506447d0fa43b84164f4b8de96663cad556c
ry00001 <ry00001@protonmail.com>
2019-05-31 20:22:58 +0100

    add some script

---
+++ Assets/ForgetInfinity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForgetInfinity : MonoBehaviour {
+	public KMSelectable[] Buttons;
+	public KMSelectable SubmitButton;
+	public KMBombModule Module;
+	public TextMesh Screen;
+
+	// Use this for initialization
+	void Start () {
+		SubmitButton.OnInteract += delegate { Submit(); return false; };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Submit() {
+		Module.HandlePass ();
+		Screen.text = "SOLVED!";
+	}
+}
